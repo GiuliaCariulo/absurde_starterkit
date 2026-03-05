db7fa1f0585ffc4ba9c6fc5fc443b9a5dd1c9eaa
GiuliaCariulo <98399966+GiuliaCariulo@users.noreply.github.com>
2026-03-05 14:05:04 +0100

    accordion

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import { gsap } from "gsap";
+
 // BURGER MENU___________________________________________--
 var burger = document.querySelector(".menu-burger");
 var menu = document.querySelector(".menu-mobile");
@@ -10,4 +12,24 @@ function toggleMenu() {
 burger.addEventListener("click", toggleMenu);
 menuItems.forEach(function (menuItem) {
   menuItem.addEventListener("click", toggleMenu);
+});
+
+// ACCORDION___________________________________________--
+
+var accordionHeaders = document.querySelectorAll(".accordion h2");
+
+accordionHeaders.forEach(function (header) {
+  header.addEventListener("click", function (event) {
+    event.stopPropagation();
+    var accordionItem =
+      this.closest(".accordion").querySelector(".accordion-item");
+    if (accordionItem.classList.contains("accordion-item-open")) {
+      accordionItem.classList.remove("accordion-item-open");
+    } else {
+      document.querySelectorAll(".accordion-item").forEach(function (item) {
+        item.classList.remove("accordion-item-open");
+      });
+      accordionItem.classList.add("accordion-item-open");
+    }
+  });
 });
